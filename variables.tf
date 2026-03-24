542512c6ee5fefe8e136367822c6aca312c65072
GilouBoubou <gilles.bourquard@accenture.com>
2026-03-24 15:11:17 +0100

    Modif AWS AMI filter

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "instance_name" {
 variable "instance_type" {
   description = "The EC2 instance's type."
   type        = string
-  default     = "t2.micro"
+  default     = "t4g.micro"
 }
 
 variable "workspace_name" {

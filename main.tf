8c7bbef39ed6488aef20235efafa5b067842879d
GilouBoubou <gilles.bourquard@accenture.com>
2026-03-26 08:54:52 +0100

    Add RSA key generation for console ssh connexion

--- main.tf
+++ main.tf
@@ -21,6 +21,7 @@ data "tfe_outputs" "source_workspace" {
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
+  key_name      = aws_key_pair.instance_keypair.key_name
 
   vpc_security_group_ids = data.tfe_outputs.source_workspace.nonsensitive_values.instance_security_group_ids
   subnet_id              = data.tfe_outputs.source_workspace.nonsensitive_values.instance_subnet
@@ -28,4 +29,20 @@ resource "aws_instance" "app_server" {
   tags = {
     Name = var.instance_name
   }
+}
+
+resource "tls_private_key" "instance_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "instance_keypair" {
+  key_name   = "${var.instance_name}-key"
+  public_key = tls_private_key.instance_key.public_key_openssh
+}
+
+resource "local_file" "private_key" {
+  content         = tls_private_key.instance_key.private_key_pem
+  filename        = "${path.module}/id_rsa_${var.instance_name}"
+  file_permission = "0600"
 }
--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,16 @@ output "instance_security_group_ids" {
 output "instance_subnet" {
   value = aws_instance.app_server.subnet_id
 }
+
+output "ssh_key_pair_name" {
+  value = aws_key_pair.instance_keypair.key_name
+}
+
+output "ssh_private_key_file" {
+  value = local_file.private_key.filename
+}
+
+output "ssh_private_key_pem" {
+  value     = tls_private_key.instance_key.private_key_pem
+  sensitive = true
+}

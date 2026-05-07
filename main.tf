b6dc60fe9d9ae8aebb49401a61906884432b7c7b
GilouBoubou <gilles.bourquard@accenture.com>
2026-05-07 16:39:26 +0200

    Move instance_subnet value in tfvars

--- main.tf
+++ main.tf
@@ -13,10 +13,12 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+/***
 data "tfe_outputs" "source_workspace" {
   workspace    = var.workspace_name
   organization = var.organization_name
 }
+***/
 
 resource "aws_instance" "app_server_1" {
   ami           = data.aws_ami.ubuntu.id
@@ -24,7 +26,8 @@ resource "aws_instance" "app_server_1" {
   key_name      = var.key_name
 
   vpc_security_group_ids = var.instance_security_group_ids
-  subnet_id              = data.tfe_outputs.source_workspace.nonsensitive_values.instance_subnet
+  // subnet_id              = data.tfe_outputs.source_workspace.nonsensitive_values.instance_subnet
+  subnet_id              = var.instance_subnet
 
   tags = {
     Name = "${var.instance_name}_1"
@@ -37,8 +40,9 @@ resource "aws_instance" "app_server_2" {
   key_name      = var.key_name
 
   vpc_security_group_ids = var.instance_security_group_ids
-  subnet_id              = data.tfe_outputs.source_workspace.nonsensitive_values.instance_subnet
-
+  // subnet_id              = data.tfe_outputs.source_workspace.nonsensitive_values.instance_subnet
+  subnet_id              = var.instance_subnet
+  
   tags = {
     Name = "${var.instance_name}_2"
   }
